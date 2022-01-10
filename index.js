0ff3c399b44f6d6a279eab9c366fb36a486afc49
Luzanne <86235334+Luzanne-S@users.noreply.github.com>
2022-01-10 18:17:27 +0200

    Updated index.js
    Edited h2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function send() {
     const information = document.getElementById("information");
 
-     information.innerHTML = "<h2>Thank you, I will contact you soon</h2>";
+     information.innerHTML = "<h2>Thank you, We will contact you soon</h2>";
      return false;
     }
